57eb300b6c98f02185d4de320f3f8ca90060211c
Bailey <Anequit@gmail.com>
2020-11-02 17:03:15 -0500

    Base project files

---
+++ Grabber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
