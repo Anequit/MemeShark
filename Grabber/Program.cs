2447123051bc9bd980ae33b4587c6e0f785ee749
Bailey <Anequit@gmail.com>
2020-11-02 17:41:26 -0500

    Fix the project

---
+++ Grabber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Grabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+            - Attach to browser in config
+		    - Manage drivers
+		    - Read from the config
+		    - Do webscrapping tasks
+		    - Write to links.json
+            **/
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
