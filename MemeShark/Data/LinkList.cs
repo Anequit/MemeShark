4528d4dbf2c3ccad2704098b4eb024b8eea884e0
Bailey <Anequit@gmail.com>
2020-11-06 05:47:42 -0500

    Handles data from the grabber

---
+++ MemeShark/Data/LinkList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MemeShark.Data
+{
+    // Read and parse data from grabber output (lists.json)
+    public class LinkList
+    {
+        public Dictionary<string, string> links { get; set; }
+    }
+}
