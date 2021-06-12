b9de04935b957ffc6b036e7692de500e447e3052
Bailey <Anequit@gmail.com>
2021-06-12 14:08:49 -0400

    Cleanup and fixing

---
+++ src/MemeShark.Core/Drivers/Edge/EdgeDriver.cs
@@ -0,0 +1,57 @@
+﻿using MemeShark.Core.Common;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace MemeShark.Core.Drivers.Edge
+{
+    class EdgeDriver : IDriver
+    {
+        public string Name { get; } = "";
+        public string Directory { get; } = "";
+
+        private EdgeOptions _options;
+        private EdgeDriverService _service;
+
+        public EdgeDriver()
+        {
+            _options = ConfigureOptions();
+            _service = ConfigureService();
+        }
+
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CheckForDriver()
+        {
+            /* if driver is present
+             *  return true
+             * 
+             * if driver isn't present
+             *  return false
+             */
+            throw new System.NotImplementedException();
+        }
+
+        private EdgeOptions ConfigureOptions()
+        {
+            EdgeOptions options = new EdgeOptions()
+            {
+                UnhandledPromptBehavior = UnhandledPromptBehavior.Dismiss,
+                PageLoadStrategy = PageLoadStrategy.Normal
+            };
+
+            return options;
+        }
+
+        private EdgeDriverService ConfigureService()
+        {
+            EdgeDriverService service = EdgeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            return service;
+        }
+
+    }
+}
