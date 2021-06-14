261e09cfc403f6c5c923604ab0f75bc59fe7c268
Bailey <Anequit@gmail.com>
2021-06-14 18:15:27 -0400

    Update EdgeDriverControllerTests.cs

--- src/MemeShark.Tests/EdgeDriverControllerTests.cs
+++ src/MemeShark.Tests/EdgeDriverControllerTests.cs
@@ -1,9 +1,4 @@
 ﻿using MemeShark.Core.Drivers.Edge;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace MemeShark.Tests
