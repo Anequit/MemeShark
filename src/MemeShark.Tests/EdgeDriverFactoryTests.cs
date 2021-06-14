f9f3344d4adbc4fae3375033f516305522c5a53f
Bailey <Anequit@gmail.com>
2021-06-14 17:17:58 -0400

    Create EdgeDriverFactoryTests.cs

---
+++ src/MemeShark.Tests/EdgeDriverFactoryTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using MemeShark.Core.Drivers.Edge;
+
+namespace MemeShark.Tests
+{
+    public class EdgeDriverFactoryTests
+    {
+        [Fact]
+        public void CheckIfDriverNameIsCorrect()
+        {
+            // Arrange 
+            string expected = "msedgedriver.exe";
+
+            // Act
+            string actual = new EdgeDriverFactory().Name;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CheckIfOptionsAreNull()
+        {
+            // Arrange
+            EdgeDriverFactory factory = new EdgeDriverFactory();
+
+            // Act
+            factory.ConfigureDriverOptions();
+
+            // Assert
+            Assert.NotNull(factory.Options);
+        }
+
+        // If this test fails then you need to place the microsoft edge driver in the bin folder of the test project.
+        // Download it at https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/ 
+        [Fact] 
+        public void CheckIfServiceIsNull()
+        {
+            // Arrange
+            EdgeDriverFactory factory = new EdgeDriverFactory();
+
+            // Act
+            factory.ConfigureDriverService();
+
+            // Assert
+            Assert.NotNull(factory.Service);
+        }
+    }
+}
