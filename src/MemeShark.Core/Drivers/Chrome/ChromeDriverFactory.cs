35ebeca47dd23682f04a05e9cd20776d4967673b
Bailey <Anequit@gmail.com>
2021-06-15 13:50:44 -0400

    Update ChromeDriverFactory.cs

--- src/MemeShark.Core/Drivers/Chrome/ChromeDriverFactory.cs
+++ src/MemeShark.Core/Drivers/Chrome/ChromeDriverFactory.cs
@@ -1,12 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using MemeShark.Core.Common;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace MemeShark.Core.Drivers.Chrome
 {
-    class ChromeDriverFactory
+    public class ChromeDriverFactory : IDriverFactory
     {
+        private ChromeOptions _options;
+        private ChromeDriverService _service;
+
+        public ChromeDriverFactory()
+        {
+            if (Name != "chromedriver.exe")
+                throw new Exception("Incorrect chrome driver name. Name should be chromedriver.exe");
+
+            ConfigureDriverOptions();
+            ConfigureDriverService();
+        }
+
+        // DO NOT CHANGE DEFAULT NAME
+        public string Name { get; } = "chromedriver.exe";
+        public string Directory { get; } = "./Drivers/";
+        public ChromeOptions Options => _options;
+        public ChromeDriverService Service => _service;
+
+        public IWebDriver Create() => new ChromeDriver(_service, _options);
+
+        public bool CheckForDriverPresence() => File.Exists(Directory + Name);
+
+        public void ConfigureDriverOptions()
+        {
+            _options = new ChromeOptions()
+            {
+                UnhandledPromptBehavior = UnhandledPromptBehavior.Dismiss,
+                PageLoadStrategy = PageLoadStrategy.Normal
+            };
+        }
+
+        public void ConfigureDriverService()
+        {
+            _service = ChromeDriverService.CreateDefaultService(Directory, Name);
+            _service.HideCommandPromptWindow = true;
+        } 
     }
 }
